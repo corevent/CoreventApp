20ef3f93b7213bdcae96737d9f9df52aa23123bb
smokeeaasd <lucasalencargomes111@gmail.com>
2026-04-07 17:38:28 -0300

    Remove MacCatalyst and iOS platform files from the project

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CoreventApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
