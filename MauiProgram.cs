f8ece34918d70d52d89f8a35d1b1b3ed7c743fdf
smokeeaasd <lucasalencargomes111@gmail.com>
2026-04-24 16:07:28 -0300

    feat: use primary font in app

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,9 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("PlusJakartaSans-Regular.ttf", "PlusJakartaSansRegular");
+				fonts.AddFont("PlusJakartaSans-SemiBold.ttf", "PlusJakartaSansSemiBold");
+				fonts.AddFont("PlusJakartaSans-Bold.ttf", "PlusJakartaSansBold");
 			});
 
 #if DEBUG
