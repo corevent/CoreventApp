4b8f9d64d71ed1658e2efbcd594ee23b4f7a474a
ViniciusCassemira <viniciuscruzc2702@gmail.com>
2026-04-30 23:40:12 -0300

    style:base screen configuration

--- Views/Profile.xaml.cs
+++ Views/Profile.xaml.cs
@@ -1,9 +1,27 @@
 namespace CoreventApp.Views;
 
+public class ProfileMenuOption
+{
+    public string ?Title { get; set; }
+}
+
 public partial class Profile : ContentPage
 {
-  public Profile()
-  {
-    InitializeComponent();
-  }
+    public List<ProfileMenuOption> Options { get; set; }
+
+    public Profile()
+    {
+        InitializeComponent();
+
+        Options = new List<ProfileMenuOption>
+        {
+            new ProfileMenuOption { Title = "Dados Pessoais" },
+            new ProfileMenuOption { Title = "Histórico de Compra" },
+            new ProfileMenuOption { Title = "Meus Favoritos" },
+            new ProfileMenuOption { Title = "Avaliações" },
+            new ProfileMenuOption { Title = "Configurações" }
+        };
+
+        BindingContext = this;
+    }
 }
