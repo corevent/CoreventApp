60db013cb943ab197547a23b3bbcabab6b8218f4
smokeeaasd <83725986+smokeeaasd@users.noreply.github.com>
2026-06-08 21:04:14 -0300

    feat: add intent filter for deep linking and update MainActivity attributes

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -4,7 +4,12 @@ using Android.OS;
 
 namespace CoreventApp;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+  Name = "com.corevent.coreventapp.MainActivity",
+  Theme = "@style/Maui.SplashTheme", 
+  MainLauncher = true, 
+  LaunchMode = LaunchMode.SingleTop, 
+  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
